4ae97ae3506580520f44f7d17b2ef0c1f8a1be45
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2019-10-03 15:37:34 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/p3karm/FrmActivityPrincipal.java
@@ -0,0 +1,13 @@
+package mx.edu.tesoem.isc.p3karm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrmActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_principal);
+    }
+}
